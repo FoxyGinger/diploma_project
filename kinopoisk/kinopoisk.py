b3f3160e78b276b7b5da583a852d5c1460701fa3
FoxyGinger <electrojanet@gmail.com>
2023-09-12 10:13:00 +0300

    bug fixes url, schema etc

--- kinopoisk/kinopoisk.py
+++ kinopoisk/kinopoisk.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+import httpx
 
 from kinopoisk.schema_checker import SchemaChecker
 
@@ -24,18 +25,22 @@ class Kinopoisk:
             token = self.__token
 
         full_url = f"{self.__main_url}/{self.__urls.get(url)}"
-        return requests.get(url=full_url, params=params, headers={"X-API-KEY": token}, timeout=15)
+        return requests.get(url=full_url, params=params, headers={
+            "Accept": "application/json",
+            "Accept-Encoding": "identity",
+            "X-API-KEY": token,
+        }, timeout=15)
 
     def movie_random(self, token: str = None) -> requests.Response:
-        return self.__get(url=self.__urls.get("random"), token=token)
+        return self.__get(url="random", token=token)
 
     def check_response_code(self, response: requests.Response, expected_code: int):
         if response.status_code != expected_code:
-            pytest.fail(f'Response "{response.url}" status code({response.status_code}) != expected_code({expected_code})')
+            pytest.fail(
+                f'Response "{response.url}" status code({response.status_code}) != expected_code({expected_code})')
 
     def check_response_body(self, response: requests.Response, schema_name: str, nested_keys: list = None):
         self.check_response_code(response, expected_code=200)
-        if not self.__schema_checker.check_field_keys(schema_name=schema_name, data=response.json(), nested_keys=nested_keys):
+        if not self.__schema_checker.check_field_keys(schema_name=schema_name, data=response.json(),
+                                                      nested_keys=nested_keys):
             pytest.fail(f'Invalid response body "{response.url}"')
-
-
